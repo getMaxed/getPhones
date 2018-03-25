5cdd84efb53df28ac523e582d6aefbea700f43f5
getmaxed <maxfaden07@gmail.com>
2018-03-25 10:30:58 -0700

    basket page done

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import './main.css'
+
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {createStore, applyMiddleware} from 'redux'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
-import {browserHistory, Router, Route} from 'react-router'
 import {syncHistoryWithStore} from 'react-router-redux'
+import {Router, Route, browserHistory} from 'react-router'
 import {Provider} from 'react-redux'
 
+import reducers from 'reducers'
 import Layout from 'containers/layout'
 import Phones from 'containers/phones'
 import Phone from 'containers/phone'
-
-import reducers from 'reducers'
-// import Provider from "react-redux/src/components/Provider";
+import Basket from 'containers/basket'
 
 const store = createStore(reducers, composeWithDevTools(
     applyMiddleware(thunk)
@@ -23,12 +23,13 @@ const history = syncHistoryWithStore(browserHistory, store);
 
 ReactDOM.render(
     <Provider store={store}>
-        <Router history={history} >
+        <Router history={history}>
             <Route component={Layout}>
-                <Route path="/" component={Phones} />
-                <Route path="/categories/:id" component={Phones} />
+                <Route path="/" component={Phones}/>
+                <Route path="/categories/:id" component={Phones}/>
             </Route>
-            <Route path='/phones/:id' component={Phone} />
+            <Route path='/phones/:id' component={Phone}/>
+            <Route path="/basket" component={Basket} />
         </Router>
     </Provider>,
     document.getElementById('root')
